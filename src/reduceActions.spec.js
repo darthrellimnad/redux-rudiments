16fefd3342f52e16b261fa7eec48eb4dc08885a9
Daniel Miller <dmillerh@gmail.com>
2019-07-11 13:52:42 -0700

    fix typos and added exception test in reduceActions.spec.js

--- src/reduceActions.spec.js
+++ src/reduceActions.spec.js
@@ -3,12 +3,13 @@ import { reduceActions } from './index'
 describe('reduceActions', () => {
   const testReducer = (state = 0, action) =>
     action.type === 'test' ? state + action.payload : state
+
   const testActions = [
     { type: 'test', payload: 1 },
     { type: 'test', payload: 2 }
   ]
 
-  test('produces array of states, given arracy of actions and reducer', () => {
+  test('produces array of states, given array of actions and reducer', () => {
     const states = reduceActions(testActions)(testReducer)
     expect(states).toEqual([0, 1, 3])
   })
@@ -16,5 +17,15 @@ describe('reduceActions', () => {
   test('called with inverted arguments', () => {
     const states = reduceActions(testReducer)(testActions)
     expect(states).toEqual([0, 1, 3])
+  })
+
+  test('throws error when non-function provided as reducer', () => {
+    expect(() => {
+      reduceActions(null)(testActions)
+    }).toThrow()
+
+    expect(() => {
+      reduceActions(testActions)(null)
+    }).toThrow()
   })
 })
